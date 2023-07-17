1addb9c83763c183e30c37d2a396b2534ed814d1
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-07-17 18:18:57 +0900

    Feat : supabase 추가

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,26 @@
+// Supabase 인증과 세션 관리를 간편하게 구현할 수 있도록 도와주는 컴포넌트
+"use client";
+
+import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+import { Database } from "@/types_db";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
+  const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+};
+
+export default SupabaseProvider;
